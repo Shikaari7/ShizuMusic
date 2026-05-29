f65e9d4b3f8140070e2ce673698a3610032179f5
ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <kiran6jeet8@gmail.com>
2026-05-29 10:12:23 +0530

    Update __init__.py

--- ShizuMusic/modules/__init__.py
+++ ShizuMusic/modules/__init__.py
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------
 #  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
+#  Developed by Bad Munda 💞
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
