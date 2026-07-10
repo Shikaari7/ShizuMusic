7943c0600e66fd053a0a3d15ce39f6a9f198c5bd
Shikaari7 <foodiebabbar@gmail.com>
2026-07-11 02:35:22 +0530

    Update project name and developer credits

--- ShizuMusic/core/__init__.py
+++ ShizuMusic/core/__init__.py
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
+#  ShilaariMusic © 2026
+#  Developed by SHIKAARI BOTS ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
